eb0fe9c9d52e8d64f7b09ef48eda641fec13d661
Giorgos Logiotatidis <seadog@sealabs.net>
2016-06-29 15:23:43 +0300

    Copy production variables into zip.

--- lambda/makezip.sh
+++ lambda/makezip.sh
@@ -7,6 +7,7 @@ TMP_DIR=`mktemp -d`
 pushd $TMP_DIR
 pip install -r $CURRENT_DIR/requirements.txt -t .
 cp -ra $CURRENT_DIR/app/. .
+cp -rp $CURRENT_DIR/app/.env.production .env
 zip -r $CURRENT_DIR/lambda.zip .
 popd
 rm -rf $TMP_DIR
