9adfd94d2cf0b4610ab70f865d04c520e047b7a8
mozmar-statusbot <statusbot@mozmar.org>
2016-08-26 10:22:57 +0000

    Site update

--- bin/update-stage.sh
+++ bin/update-stage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ -z "${DEVELOP_REPOSITORY}" ]]; then
-    echo "Set DEVELOP_REPOSITORY"
+if [[ -z "${STAGING_REPOSITORY}" ]]; then
+    echo "Set STAGING_REPOSITORY"
     exit 1;
 fi
 
@@ -11,24 +11,32 @@ if [[ -z "${GITHUB_AUTH_TOKEN}" ]]; then
     exit 1;
 fi
 
-git clone https://github.com/${DEVELOP_REPOSITORY} ~/develop
+PRESERVE_FILES="./docs/CNAME ./docs/status.yml"
 
+git clone https://github.com/${STAGING_REPOSITORY} ~/develop
 
 # Delete all files from develop repo except from the git dir
 pushd ~/develop
-find . -maxdepth 1  -not -path './.git' -not -path '.' -exec rm -rf {} \;
+TMP=$(mktemp)
+
+# Tar the files to be preserved
+tar cf $TMP ${PRESERVE_FILES}
+
+# Delete everything except .git directory
+find . -maxdepth 1 -not -path "./.git" -not -path '.' -exec rm -rf {} \;
 popd
+
+# Copy everything except git directory
 find . -maxdepth 1  -not -path './.git' -not -path '.' -exec cp -r {} ~/develop \;
 pushd ~/develop
 
-# Add CNAME file for GitHub pages.
-CNAME_PATH=${CNAME_PATH:-./docs/CNAME}
-mkdir -p $(dirname ${CNAME_PATH})
-echo ${CNAME} > ${CNAME_PATH}
+# Restore preserved files
+tar xf ${TMP}
 
+# Commit to staging repository
 git config --global user.email "statusbot@mozmar.org"
 git config --global user.name "mozmar-statusbot"
 git add .
 git commit -m "Site update"
-git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/${DEVELOP_REPOSITORY}
+git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/${STAGING_REPOSITORY}
 git push -f origin-rw master
