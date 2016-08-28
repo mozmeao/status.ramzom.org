fdf38b6041d2d86181fce29b95d1aa5a2cce1031
mozmar-statusbot <statusbot@mozmar.org>
2016-08-28 02:08:51 +0000

    Site update

--- makezip.sh
+++ makezip.sh
@@ -4,14 +4,16 @@ set -e
 CURRENT_DIR=`pwd`
 TMP_DIR=`mktemp -d`
 GIT_SHA=`git rev-parse HEAD`
-rm -f $CURRENT_DIR/lambda.zip
+ENVIRONMENT=${ENVIRONMENT:-staging}
+ZIP_FILENAME="${CURRENT_DIR}/lambda-${ENVIRONMENT}.zip"
 
-pushd $TMP_DIR
-pip install -r $CURRENT_DIR/requirements.txt -t .
-cp -ra $CURRENT_DIR/app/* .
-cp -rp $CURRENT_DIR/app/.env.production .env
-echo GIT_SHA=$GIT_SHA >> .env
+rm -f ${ZIP_FILENAME}
+pushd ${TMP_DIR}
+pip install -r ${CURRENT_DIR}/requirements.txt -t .
+cp -ra ${CURRENT_DIR}/app/* .
+cp -rp ${CURRENT_DIR}/app/.env.${ENVIRONMENT} .env
+echo GIT_SHA=${GIT_SHA} >> .env
 find . -name \*.pyc -delete
-zip -r $CURRENT_DIR/lambda.zip .
+zip -r ${ZIP_FILENAME} .
 popd
-rm -rf $TMP_DIR
+rm -rf ${TMP_DIR}
